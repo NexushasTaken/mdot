d69e0aefb6d10b7284684cff0843afd552b615ea
NexushasTaken <nexushastaken@gmail.com>
2026-02-17 23:59:28 +0800

    test(hybrid): add test for optional union

--- tests/hybrid/test_union_types.lua
+++ tests/hybrid/test_union_types.lua
@@ -23,4 +23,45 @@ function testUnionWithBuiltinTypes()
    lu.assertEquals(err, "")
    _, err = test(nil)
    lu.assertEquals(err, "")
+
+   test = t.union(t.string, t.number, nil)
+   _, err = test(1)
+   lu.assertEquals(err, "")
+   _, err = test("")
+   lu.assertEquals(err, "")
+   _, err = test(nil)
+   lu.assertEquals(err, "expected type string or number, got nil")
+end
+
+function testOptionalUnionWithBuiltinTypes()
+   ---@type Result, string
+   local _, err
+
+   local test
+
+   test = t.union(t.string, t.number):optional()
+   _, err = test(1)
+   lu.assertEquals(err, "")
+   _, err = test("")
+   lu.assertEquals(err, "")
+   _, err = test(true)
+   lu.assertEquals(err, "expected type optional string or number, got boolean: true")
+
+   test = t.union(t.string, t.number, t.any):optional()
+   _, err = test(1)
+   lu.assertEquals(err, "")
+   _, err = test("")
+   lu.assertEquals(err, "")
+   _, err = test(true)
+   lu.assertEquals(err, "")
+   _, err = test(nil)
+   lu.assertEquals(err, "")
+
+   test = t.union(t.string, t.number, nil):optional()
+   _, err = test(1)
+   lu.assertEquals(err, "")
+   _, err = test("")
+   lu.assertEquals(err, "")
+   _, err = test(nil)
+   lu.assertEquals(err, "")
 end
