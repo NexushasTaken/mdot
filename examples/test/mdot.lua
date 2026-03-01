ed727145157c2896af50e1854d474a81d10d207d
NexushasTaken <nexushastaken@gmail.com>
2026-03-01 19:10:10 +0800

    docs: add examples from master-old branch

---
+++ examples/test/mdot.lua
@@ -0,0 +1,20 @@
+return {
+  "ly",
+  "fish",
+  {
+    name = "hypr"
+  },
+  hypr = {
+    depends = {
+      "fish",
+      "neovim",
+      "uwsm",
+    },
+    exclude = "*",
+  },
+  git = {
+    depends = {
+      "hyprland",
+    },
+  },
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod specs;
 use logger::setup_logger;
 
 use log::{debug, error, info, trace, warn};
-use mlua::Table;
+use mlua::{StdLib, Table};
 use specs::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_logger()?;
     let mut ctx = Context::default();
+
+    ctx.lua.load_std_libs(StdLib::PACKAGE)?;
 
     let source: String = r#"
     return {
